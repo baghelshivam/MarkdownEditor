bc3e7a0e5e0822018e1cfede213165c9b5123db5
baghelshivam <baghelshivam11@gmail.com>
2023-03-22 23:30:50 +0530

    Entry Point.

---
+++ server.js
@@ -0,0 +1,10 @@
+express = require("express");
+
+const app = express();
+app.use(express.static("/public"));
+
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+  console.log(`Listening on ${PORT} `);
+});
