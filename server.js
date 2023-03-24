9b48cacf6620fe3dce2eac76cc3ab6ba65369b49
baghelshivam <baghelshivam11@gmail.com>
2023-03-24 23:47:03 +0530

    Added route

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
-express = require("express");
+const express = require("express");
+const markdown = require("./routes/markdown");
 
 const app = express();
-app.use(express.static("/public"));
-
 const PORT = process.env.PORT || 3001;
 
+app.use("/markdown",markdown);
+app.use(express.static(__dirname + "/public"));
+
 app.listen(PORT, () => {
-  console.log(`Listening on ${PORT} `);
+  console.log(`Listning on the Port ${PORT}`);
 });
